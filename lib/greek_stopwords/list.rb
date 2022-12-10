68c59513398f151592af8561d72b615ce2109a62
Dimitrios Douros <dourosdimitris@outlook.com>
2022-12-10 18:56:49 +0200

    fixed returning array

--- lib/greek_stopwords/list.rb
+++ lib/greek_stopwords/list.rb
@@ -3,27 +3,27 @@
 module GreekStopwords
   class List
     def initialize
-      antonymies = "#{File.dirname(__FILE__)}/parts_of_speech/antonymies.txt"
-      arithmhtika = "#{File.dirname(__FILE__)}/parts_of_speech/arithmhtika.txt"
-      arthra = "#{File.dirname(__FILE__)}/parts_of_speech/arthra.txt"
-      epifonhmata = "#{File.dirname(__FILE__)}/parts_of_speech/epifonhmata.txt"
-      epirrhmata = "#{File.dirname(__FILE__)}/parts_of_speech/epirrhmata.txt"
-      moria = "#{File.dirname(__FILE__)}/parts_of_speech/moria.txt"
-      protheseis = "#{File.dirname(__FILE__)}/parts_of_speech/protheseis.txt"
-      syndesmoi = "#{File.dirname(__FILE__)}/parts_of_speech/syndesmoi.txt"
-
-      
-      @antonymies = File.read(antonymies).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
-      @arithmhtika = File.read(arithmhtika).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
-      @arthra = File.read(arthra).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
-      @epifonhmata = File.read(epifonhmata).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
-      @epirrhmata = File.read(epirrhmata).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
-      @moria = File.read(moria).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
-      @protheseis = File.read(protheseis).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
-      @syndesmoi = File.read(syndesmoi).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
+      @antonymies_str = "#{File.dirname(__FILE__)}/parts_of_speech/antonymies.txt"
+      @arithmhtika_str = "#{File.dirname(__FILE__)}/parts_of_speech/arithmhtika.txt"
+      @arthra_str = "#{File.dirname(__FILE__)}/parts_of_speech/arthra.txt"
+      @epifonhmata_str = "#{File.dirname(__FILE__)}/parts_of_speech/epifonhmata.txt"
+      @epirrhmata_str = "#{File.dirname(__FILE__)}/parts_of_speech/epirrhmata.txt"
+      @moria_str = "#{File.dirname(__FILE__)}/parts_of_speech/moria.txt"
+      @protheseis_str = "#{File.dirname(__FILE__)}/parts_of_speech/protheseis.txt"
+      @syndesmoi_str = "#{File.dirname(__FILE__)}/parts_of_speech/syndesmoi.txt"
+      return 'Loaded'
     end
 
     def all
+      @antonymies = File.read(@antonymies_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
+      @arithmhtika = File.read(@arithmhtika_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
+      @arthra = File.read(@arthra_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
+      @epifonhmata = File.read(@epifonhmata_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
+      @epirrhmata = File.read(@epirrhmata_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
+      @moria = File.read(@moria_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
+      @protheseis = File.read(@protheseis_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
+      @syndesmoi = File.read(@syndesmoi_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
+
       @all = @antonymies + @arithmhtika + @arthra + @epifonhmata + @epirrhmata + @moria + @protheseis + @syndesmoi
       @all.uniq
     end
