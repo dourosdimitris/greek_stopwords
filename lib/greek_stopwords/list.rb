2348fe420b02fbb1f152230d23e4c4d75886b617
Dimitrios Douros <dourosdimitris@outlook.com>
2022-12-10 19:00:52 +0200

    added verbs & punctuation

--- lib/greek_stopwords/list.rb
+++ lib/greek_stopwords/list.rb
@@ -11,6 +11,8 @@ module GreekStopwords
       @moria_str = "#{File.dirname(__FILE__)}/parts_of_speech/moria.txt"
       @protheseis_str = "#{File.dirname(__FILE__)}/parts_of_speech/protheseis.txt"
       @syndesmoi_str = "#{File.dirname(__FILE__)}/parts_of_speech/syndesmoi.txt"
+      @rhmata_str = "#{File.dirname(__FILE__)}/parts_of_speech/rhmata.txt"
+      @punctuation_str = "#{File.dirname(__FILE__)}/parts_of_speech/punctuation.txt"
       return 'Loaded'
     end
 
@@ -23,8 +25,10 @@ module GreekStopwords
       @moria = File.read(@moria_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
       @protheseis = File.read(@protheseis_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
       @syndesmoi = File.read(@syndesmoi_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
+      @rhmata = File.read(@rhmata_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
+      @punctuation = File.read(@punctuation_str).split("\n").select{ |item| item unless item.empty? or item.include?("#") }
 
-      @all = @antonymies + @arithmhtika + @arthra + @epifonhmata + @epirrhmata + @moria + @protheseis + @syndesmoi
+      @all = @antonymies + @arithmhtika + @arthra + @epifonhmata + @epirrhmata + @moria + @protheseis + @syndesmoi + @rhmata + @punctuation
       @all.uniq
     end
   end
